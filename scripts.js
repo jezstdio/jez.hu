9551a0d9f6e31b846ef90b45ef0852706d413e37
jezstdio <jezstdio@protonmail.com>
2021-12-28 17:56:06 +0000

    initial commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function setSiteHeight() {
+    const root = document.documentElement;
+
+    root.style.setProperty('--site-height', `${window.innerHeight}px`);
+}
+
+setSiteHeight();
+window.addEventListener('resize', setSiteHeight);
